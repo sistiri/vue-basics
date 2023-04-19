1f91422822ca69fb86b350f4c880f19e159d6edf
Gabor.Nemes <Gabor.Nemes@kyndryl.com>
2023-04-20 01:33:55 +0200

    Props Binding

--- 02-components/01-create-components/src/App.vue
+++ 02-components/01-create-components/src/App.vue
@@ -1,11 +1,18 @@
 <script setup>
+import { ref } from 'vue'
 import MainNavigation from './components/MainNavigation.vue'
-import SiteContent from './components/SiteContent.vue';
-import SiteFooter from './components/SiteFooter.vue';
+import SiteContent from './components/SiteContent.vue'
+import SiteFooter from './components/SiteFooter.vue'
+
+const title = ref('VUE')
+
+// setTimeout(() => {
+//   title.value="REACT"
+// }, 5000);
 </script>
 
 <template>
-  <MainNavigation title="Vue.js"/>
+  <MainNavigation :title="title" />
   <SiteContent />
   <SiteFooter />
 </template>
