23bd70cf63a8fdc120f2157c550c17b35e0337dd
Gabor.Nemes <Gabor.Nemes@kyndryl.com>
2023-04-24 15:52:48 +0200

    Vue Lifecycle Hooks

--- 02-components/vue-lifecycle-hooks/src/App.vue
+++ 02-components/vue-lifecycle-hooks/src/App.vue
@@ -1,11 +1,14 @@
 <script setup>
+import { ref } from 'vue'
+import AppTitle from './components/AppTitle.vue'
 
+const isTitleVisible = ref(true)
+
+setTimeout(() => (isTitleVisible.value = false), 5000)
 </script>
 
 <template>
- 
+  <AppTitle v-if="isTitleVisible" />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
